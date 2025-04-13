70e0e3e59f77543f432eafc0591ae5e248a1a00b
Aayush Badola <aayush.badola2@gmail.com>
2025-04-13 22:08:38 +0530

    Update aquant.h

--- aquant.h
+++ aquant.h
@@ -1,9 +1,7 @@
 #ifndef AQUANT_H
 #define AQUANT_H
 
-#include <stddef.h> // Required for size_t potentially used by underlying functions
-
-// Define string type alias like CS50
+#include <stddef.h> 
 typedef char *string;
 
 // Function prototypes
@@ -14,4 +12,4 @@ int get_int(const char *prompt);
 long get_long(const char *prompt);
 string get_string(const char *prompt);
 
-#endif // AQUANT_H
+#endif 
